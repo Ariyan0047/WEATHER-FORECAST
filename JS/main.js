cbad6d4a01a51ae762627e21f7f3f78456cf3a73
Ariyan0047 <aariyan0047@gmail.com>
2020-12-18 00:16:35 +0600

    FIFTH COMMIT

--- JS/main.js
+++ JS/main.js
@@ -4,10 +4,10 @@ const apiKey = "tNnx8QJPdQZgWyeoOUbhPLeYgFq6rifn"
 
 // GETTING CITY INFORMATION FROM ACCU WEATHER
 const getCityInfo = async (cityName) => {
-    const countryKey = `http://dataservice.accuweather.com/locations/v1/cities/search`
-    const query = `?apikey=${apiKey}&q=${cityName}`
-    const fetchingData = `${countryKey}${query}`
-    const response = await fetch(fetchingData)
+    const countryKey = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
+    // const query = `?apikey=${apiKey}&q=${cityName}`
+    // const fetchingData = `${countryKey}${query}`
+    const response = await fetch(countryKey)
     if (response.status !== 200) {
         alert("COUNTRY NAME INCORRECT !!!!!!!!!!!!!")
     }
@@ -17,10 +17,10 @@ const getCityInfo = async (cityName) => {
 
 // GETTING CITY WEATHER CONDITION
 const getWeatherCondition = async (cityKey) => {
-    const weatherKey = "http://dataservice.accuweather.com/currentconditions/v1/"
-    const query = `${cityKey}?apikey=${apiKey}`
-    const fetchingData = `${weatherKey}${query}`
-    const response = await fetch(fetchingData)
+    const weatherKey = `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`
+    // const query = `${cityKey}?apikey=${apiKey}`
+    // const fetchingData = `${weatherKey}${query}`
+    const response = await fetch(weatherKey)
     const data = await response.json()
     return data[0]
 }
